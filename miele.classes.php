9ec27efe7b2bdf8c962214296cc03d03f712563d
root <root@Server.localdomain>
2023-05-06 13:52:40 +0200

    lot of changes to stay connected to the api. added setValue() command to change Values using the api

--- miele.classes.php
+++ miele.classes.php
@@ -18,15 +18,14 @@ class MieleRest
     private $authorizationCode = '';
     private $accessToken = '';
     private $refreshToken = '';
+    private $tokenExpiresAt = '';
     private $deviceData = array();
 
-    public function __construct(MieleSettings $settings)
-    {
+    public function __construct(MieleSettings $settings){
         $this->settings = $settings;
     }
 
-    private function curlRequest($url, $method, $contentType, $data, $token, &$responseCode)
-    {
+    private function curlRequest($url, $method, $contentType, $data, $token, &$responseCode){
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -40,8 +39,7 @@ class MieleRest
 
         $result = curl_exec($ch);
         $returnValue = false;
-        if($result !== false)
-        {
+        if($result !== false){
             $responseCode = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
             if($responseCode == 302) $returnValue = curl_getinfo($ch, CURLINFO_REDIRECT_URL);
             else if($responseCode >= 200 && $responseCode < 300) $returnValue = $result;
@@ -50,137 +48,173 @@ class MieleRest
 	    return $returnValue;
     }
 
-    private function login()
-    {
+    private function calculateTokenExpireDate($expiresIn){
+        $date = new DateTime();
+        $date->add(new DateInterval('PT'.$expiresIn.'S'));
+        HomegearNodeBase::log(4, 'token expires in: '. $expiresIn. ' so at: '. date_format($date, 'Y-m-d H:i:s'));
+        return $date->getTimestamp();
+    }
+
+    private function login(){
         $this->accessToken = '';
-        if($this->refreshToken && $this->settings->clientId && $this->settings->clientSecret)
-        {
+        if($this->refreshToken && $this->settings->clientId && $this->settings->clientSecret){
             $url = 'https://api.mcs3.miele.com/thirdparty/token?client_id='.urlencode($this->settings->clientId).'&client_secret='.urlencode($this->settings->clientSecret).'&refresh_token='.urlencode($this->refreshToken).'&redirect_uri=%2Fv1%2Fdevices&grant_type=refresh_token&state=token';
             $responseCode = 0;
             $result = $this->curlRequest($url, 'GET', '', '', '', $responseCode);
-            if($result === false || $responseCode == 0) return false;
-            else if($responseCode == 200)
-            {
+            if($result === false || $responseCode == 0){
+                HomegearNodeBase::log(2, 'Unknown error using refresh token. (response code '.$responseCode.'): '.$result);
+                //todo check if this works. i think returning false here result in a loop with trying to use refresh token that is no more usable.
+                //return false;
+            }
+            else if($responseCode == 200){
                 $tokens = json_decode($result, true);
                 $this->accessToken = $tokens['access_token'] ?? '';
                 $this->refreshToken = $tokens['refresh_token'] ?? '';
-                if($this->accessToken)
-                {
-                    HomegearNodeBase::log(4, 'Successfully refreshed access token.');
+                $this->tokenExpiresAt = $this->calculateTokenExpireDate($tokens['expires_in']);
+                
+                if($this->accessToken){
+                    HomegearNodeBase::log(4, 'Successfully refreshed access token. (response code '.$responseCode.'): '.$result);
                     return true;
                 }
             }
         }
 
-        if($this->settings->userId && $this->settings->password && $this->settings->clientId && $this->settings->clientSecret && $this->settings->country)
-        {
+        if($this->settings->userId && $this->settings->password && $this->settings->clientId && $this->settings->clientSecret && $this->settings->country){
             $this->authorizationCode = '';
 
             $url = 'https://api.mcs3.miele.com/oauth/auth';
             $data = 'email='.urlencode($this->settings->userId).'&password='.urlencode($this->settings->password).'&redirect_uri=%2Fv1%2Fdevices&state=login&response_type=code&client_id='.urlencode($this->settings->clientId).'&vgInformationSelector='.urlencode($this->settings->country);
             $responseCode = 0;
             $result = $this->curlRequest($url, 'POST', 'application/x-www-form-urlencoded', $data, '', $responseCode);
-            if($result === false || $responseCode == 0)
-            {
-                HomegearNodeBase::log(2, 'Unknown error obtaining authorization code. E. g. server is not reachable.');
+            if($result === false || $responseCode == 0){
+                HomegearNodeBase::log(2, 'Unknown error obtaining authorization code. (response code '.$responseCode.'): '.$result);
                 return false;
             }
-            else if($responseCode == 302)
-            {
-                if(is_string($result))
-                {
+            else if($responseCode == 302){
+                if(is_string($result)){
                     $parameters = explode('?', $result);
-                    if(count($parameters) > 1)
-                    {
+                    if(count($parameters) > 1){
                         $parameters = $parameters[1];
                         $parameters = explode('&', $parameters);
-                        foreach($parameters as $parameter)
-                        {
+                        foreach($parameters as $parameter){
                             $parameter = explode('=', $parameter);
                             if(count($parameter) == 2 && $parameter[0] == 'code') $this->authorizationCode = $parameter[1];
+                            HomegearNodeBase::log(4, 'Successfully get authorizationCode (response code '.$responseCode.'): '.$result);
                         }
                     }
                 }
             }
-            else
-            {
+            else{
                 HomegearNodeBase::log(2, 'Error obtaining authorization code (response code '.$responseCode.'): '.$result);
             }
 
-            if($this->authorizationCode)
-            {
+            if($this->authorizationCode){
                 $url = 'https://api.mcs3.miele.com/thirdparty/token?client_id='.urlencode($this->settings->clientId).'&client_secret='.urlencode($this->settings->clientSecret).'&code='.urlencode($this->authorizationCode).'&redirect_uri=%2Fv1%2Fdevices&grant_type=authorization_code&state=token';
                 $responseCode = 0;
                 $result = $this->curlRequest($url, 'POST', 'application/x-www-form-urlencoded', '', '', $responseCode);
-                if($result === false || $responseCode == 0)
-                {
-                    HomegearNodeBase::log(2, 'Unknown error obtaining access token. E. g. server is not reachable.');
+                if($result === false || $responseCode == 0){
+                    HomegearNodeBase::log(2, 'Unknown error obtaining access token. (response code '.$responseCode.'): '.$result);
                     return false;
                 }
-                else if($responseCode == 200)
-                {
+                else if($responseCode == 200){
                     $tokens = json_decode($result, true);
                     $this->accessToken = $tokens['access_token'] ?? '';
                     $this->refreshToken = $tokens['refresh_token'] ?? '';
-                    if($this->accessToken)
-                    {
-                        HomegearNodeBase::log(4, 'Successfully obtained access token.');
+                    $this->tokenExpiresAt = $this->calculateTokenExpireDate($tokens['expires_in']);
+
+                    if($this->accessToken){
+                        HomegearNodeBase::log(4, 'Successfully obtained access token. (response code '.$responseCode.'): '.$result);
                         return true;
                     }
                 }
-                else
-                {
+                else{
                     HomegearNodeBase::log(2, 'Error obtaining access token (response code '.$responseCode.'): '.$result);
                     return false;
                 }
             }
         }
-        else
-        {
+        else{
             HomegearNodeBase::log(2, 'Node is not fully configured.');
         }
 
         return false;
     }
 
-    private function restRequest($url, $method, $contentType = '', $data = '')
-    {
-        if(!$this->accessToken)
-        {
+    private function refreshToken(){
+        $url = 'https://api.mcs3.miele.com/thirdparty/token';
+        $data = 'client_id='. urlencode($this->settings->clientId). '&client_secret='. urlencode($this->settings->clientSecret). '&refresh_token='. $this->refreshToken . '&grant_type=refresh_token';
+	    $responseCode = 0;
+        $result = $this->curlRequest($url, 'POST', 'application/x-www-form-urlencoded', $data, '', $responseCode);
+        if($result === false || $responseCode == 0){
+            HomegearNodeBase::log(2, 'Unknown error refreshing token. (response code '.$responseCode.'): '.$result);
+            return false;
+        }
+        else if($responseCode == 200){
+            $tokens = json_decode($result, true);
+            $this->accessToken = $tokens['access_token'] ?? '';
+            $this->refreshToken = $tokens['refresh_token'] ?? '';
+            $this->tokenExpiresAt = $this->calculateTokenExpireDate($tokens['expires_in']);
+
+            if($this->accessToken){
+                HomegearNodeBase::log(4, 'Successfully refreshed access token (response code '.$responseCode.'): '.$result);
+                return true;
+            }
+        }
+        else{
+            HomegearNodeBase::log(2, 'Error refreshing access token (response code '.$responseCode.'): '.$result);
+            return false;
+        }
+    }
+
+    private function restRequest($url, $method, $contentType = '', $data = '', &$responseCode = 0){
+        if(!$this->accessToken){
             if($this->login() === false) return false;
         }
 
-        $responseCode = 0;
         $result = $this->curlRequest($url, $method, $contentType, $data, $this->accessToken, $responseCode);
-        if($result === false || $responseCode == 0)
-        {
-            HomegearNodeBase::log(2, 'Unknown error calling URL '.$url.'. E. g. server is not reachable.');
+        if($result === false || $responseCode == 0){
+            HomegearNodeBase::log(2, 'Unknown error calling URL '.$url.'. (response code '.$responseCode.'): '.$result);
             return false;
         }
-        else if($responseCode == 401)
-        {
+        else if($responseCode == 401){
+            HomegearNodeBase::log(2, 'Error 401 calling URL '.$url.' trying to login again');
             if($this->login() === false) return false;
             $result = $this->curlRequest($url, $method, $contentType, $data, $this->accessToken, $responseCode);
         }
 
         if($responseCode >= 200 && $responseCode < 300) return json_decode($result, true);
-        else
-        {
+        else{
             HomegearNodeBase::log(2, 'Error calling URL '.$url.' (response code '.$responseCode.'): '.$result);
         }
 
         return false;
     }
 
-    public function getDevices()
-    {
-	    return $this->restRequest('https://api.mcs3.miele.com/v1/devices/'.($this->settings->language ? '?language='.urlencode($this->settings->language) : ''), 'GET');
+    public function logout(){
+        $url = 'https://api.mcs3.miele.com/thirdparty/logout';
+        $responseCode = 0;
+        $result = $this->curlRequest($url, 'POST', 'application/x-www-form-urlencoded', '', $this->accessToken, $responseCode);
+        
+        if($result === false || $responseCode == 0){
+            HomegearNodeBase::log(2, 'Unknown error calling URL '.$url.'. (response code '.$responseCode.'): '.$result);
+            return false;
+        }
+        else if($responseCode == 204){
+            HomegearNodeBase::log(4, 'Successfully logged out (response code '.$responseCode.'): '.$result);
+            $this->accessToken = '';
+            $this->refreshToken = '';
+            $this->tokenExpiresAt = '';
+            return true;
+        }
     }
 
-    public function processDevices($devices)
-    {
-        foreach ($devices as $device)
-        {
+    public function getDevices(){
+        $responseCode = 0;
+	    return $this->restRequest('https://api.mcs3.miele.com/v1/devices/'.($this->settings->language ? '?language='.urlencode($this->settings->language) : ''), 'GET', '', $responseCode = 0);
+    }
+
+    public function processDevices($devices){
+        foreach ($devices as $device){
             $deviceData = array();
             $serialNumber = $device['ident']['deviceIdentLabel']['fabNumber'] ?? '';
             if(!$serialNumber) continue;
@@ -193,16 +227,31 @@ class MieleRest
 
             $url='https://api.mcs3.miele.com/v1/devices/'. $device . '/events' .($this->settings->language ? '?language='.urlencode($this->settings->language) : '');
             $method='GET';
+            //fixme: $devive must be string not struct
             //$data = $this->restRequest($url, $method, '', '');
             //\Homegear\Homegear::nodeOutput($this->settings->nodeId, 0, array('payload' => $data));
 
 
-            if(isset($this->deviceData[$serialNumber]) && isset($this->deviceData[$serialNumber]['hash']) && $this->deviceData[$serialNumber]['hash'] == $deviceData['hash'])
-            {
+            if(isset($this->deviceData[$serialNumber]) && isset($this->deviceData[$serialNumber]['hash']) && $this->deviceData[$serialNumber]['hash'] == $deviceData['hash']){
                 //continue;
+                //fixme: do something that a device with no changes since the last poll would call nodeOutput()
             }
             $this->deviceData[$serialNumber] = $deviceData;
             \Homegear\Homegear::nodeOutput($this->settings->nodeId, 0, array('payload' => $deviceData));
+        }
+    }
+
+    public function checkTocken(){
+        if(!$this->tokenExpiresAt) return false;
+        $date = new DateTime();
+        $timeRemaining = $this->tokenExpiresAt - ($date->getTimestamp());
+
+        HomegearNodeBase::log(5, 'The token can be used for '. $timeRemaining. 's until it need\'s to be refreshed');
+
+        if($timeRemaining < 86400){ // less than one day
+            HomegearNodeBase::log(4, 'The token can be used for less than one Day, so we try to refresh');
+            $this->refreshToken();
+            return true;
         }
     }
 
@@ -214,9 +263,30 @@ class MieleRest
         $url='https://api.mcs3.miele.com/v1/devices/'. $device . '/actions' .($this->settings->language ? '?language='.urlencode($this->settings->language) : '');
         $method='GET';
         
-        $data = $this->restRequest($url, $method, '', '');
-        \Homegear\Homegear::nodeOutput($this->settings->nodeId, 1, array('payload' => $data));
-        return $data;
+        $responseCode = 0;
+        $data = $this->restRequest($url, $method, '', '', $responseCode);
+        if($responseCode == 200){
+            HomegearNodeBase::log(4, 'Result of getActions(): '. $data);
+            \Homegear\Homegear::nodeOutput($this->settings->nodeId, 1, array('payload' => $data));
+        }
+        elseif($responseCode == 204){
+            $data = true;
+            HomegearNodeBase::log(4, 'Result of getActions(): '. $data);
+            \Homegear\Homegear::nodeOutput($this->settings->nodeId, 1, array('payload' => $data));
+        }
+        else {
+            HomegearNodeBase::log(2, 'Error during getActions() '.$url.' (response code '.$responseCode. ')');
+        }
+    }
+    
+    public function getEvents(){
+        $device = 'all';
+        $url='https://api.mcs3.miele.com/v1/devices/'. $device . '/events' .($this->settings->language ? '?language='.urlencode($this->settings->language) : '');
+        $method='GET';
+        
+        $responseCode = 0;
+        $data = $this->restRequest($url, $method, '', '', $responseCode);
+        \Homegear\Homegear::nodeOutput($this->settings->nodeId, 3, array('payload' => $data));
     }
 
     public function setValue($msg){
@@ -232,18 +302,17 @@ class MieleRest
         $method='PUT';
         $data = json_encode(array($action=>$state));
 
-        $data = $this->restRequest($url, $method, 'application/json', $data);
-        \Homegear\Homegear::nodeOutput($this->settings->nodeId, 0, array('payload' => $data));
-        return $data;
-    }
+        $responseCode = 0;
+        $this->restRequest($url, $method, 'application/json', $data, $responseCode);
 
-    public function getEvents(){
-        $device = 'all';
-        $url='https://api.mcs3.miele.com/v1/devices/'. $device . '/events' .($this->settings->language ? '?language='.urlencode($this->settings->language) : '');
-        $method='GET';
-        
-        $data = $this->restRequest($url, $method, '', '');
-        \Homegear\Homegear::nodeOutput($this->settings->nodeId, 2, array('payload' => $data));
-        return $data;
-    }
+        $data = false;
+        if ($responseCode == 204){
+            $data = true;
+            HomegearNodeBase::log(4, 'Result of setValue(): '. $data);
+            \Homegear\Homegear::nodeOutput($this->settings->nodeId, 2, array('payload' => $data));
+        }
+        else {
+            HomegearNodeBase::log(2, 'Error during setValue() '.$url.' (response code '.$responseCode. ')');
+        }
+    }   
 }
--- miele.s.php
+++ miele.s.php
@@ -3,13 +3,11 @@ declare(strict_types=1);
 
 use parallel\{Channel,Runtime,Events,Events\Event};
 
-$restThread = function(string $scriptId, string $nodeId, array $mieleSettings, Channel $homegearChannel)
-{
+$restThread = function(string $scriptId, string $nodeId, array $mieleSettings, Channel $homegearChannel){
     require('miele.classes.php'); //Bootstrapping in Runtime constructor does not work for some reason.
 
     $hg = new \Homegear\Homegear();
-    if($hg->registerThread($scriptId) === false)
-    {
+    if($hg->registerThread($scriptId) === false){
         $hg->log(2, "Could not register thread.");
         return;
     }
@@ -32,26 +30,18 @@ $restThread = function(string $scriptId, string $nodeId, array $mieleSettings, C
     $i = 0;
     $nextTo = rand(50, 70);
 
-    while(true)
-    {
-        try
-        {
-            if($i < $nextTo)
-            {
+    while(true){
+        try{
+            if($i < $nextTo){
                 $breakLoop = false;
                 $event = NULL;
-                do
-                {
+                do{
                     $event = $events->poll();
-                    if($event)
-                    {
-                        if($event->source == 'mainHomegearChannelNode'.$nodeId)
-                        {
+                    if($event){
+                        if($event->source == 'mainHomegearChannelNode'.$nodeId){
                             $events->addChannel($homegearChannel);
-                            if($event->type == Event\Type::Read)
-                            {
-                                if(is_array($event->value) && count($event->value) > 0)
-                                {
+                            if($event->type == Event\Type::Read){
+                                if(is_array($event->value) && count($event->value) > 0){
                                     if($event->value['name'] == 'stop') $breakLoop = true; //Stop
                                     elseif($event->value['name'] == 'setValue') $mieleRest->setValue($event->value['value']);
                                     elseif($event->value['name'] == 'getActions') $mieleRest->getActions($event->value['value']);
@@ -61,26 +51,27 @@ $restThread = function(string $scriptId, string $nodeId, array $mieleSettings, C
                             else if($event->type == Event\Type::Close) $breakLoop = true; //Stop
                         }
                     }
-
                     if($breakLoop) break;
                 }
                 while($event);
             }
 
-            if($breakLoop) break;
-
+            if($breakLoop){
+                $mieleRest->logout();
+                break;
+            }
             if($i < $nextTo) continue;
             $i = 0;
             $nextTo = rand(50, 70);
 
+            $mieleRest->checkTocken();
+
             $devices = $mieleRest->getDevices();
-            if($devices !== false)
-            {
+            if($devices !== false){
                 $mieleRest->processDevices($devices);
             }
         }
-        catch(Events\Error\Timeout $ex)
-        {
+        catch(Events\Error\Timeout $ex){
             $i++;
         }
     }
@@ -94,25 +85,21 @@ class HomegearNode extends HomegearNodeBase
     private $mainFuture = NULL;
     private $mainHomegearChannel = NULL; //Channel to pass Homegear events to main thread
 
-    function __construct()
-    {
+    function __construct(){
         $this->hg = new \Homegear\Homegear();
     }
 
-    function __destruct()
-    {
+    function __destruct(){
         $this->stop();
         $this->waitForStop();
     }
 
-    public function init(array $nodeInfo) : bool
-    {
+    public function init(array $nodeInfo) : bool{
         $this->nodeInfo = $nodeInfo;
         return true;
     }
 
-    public function start() : bool
-    {
+    public function start() : bool{
         $scriptId = $this->hg->getScriptId();
         $nodeId = $this->nodeInfo['id'];
         
@@ -132,8 +119,7 @@ class HomegearNode extends HomegearNodeBase
         return true;
     }
 
-    public function input(array $nodeInfoLocal, int $inputIndex, array $message)
-    {
+    public function input(array $nodeInfoLocal, int $inputIndex, array $message){
         if($this->mainHomegearChannel){
             if($inputIndex == 0) $this->mainHomegearChannel->send(['name' => 'getActions', 'value' => $message['payload']]);
             elseif($inputIndex == 1) $this->mainHomegearChannel->send(['name' => 'setValue', 'value' => $message['payload']]);
@@ -141,27 +127,22 @@ class HomegearNode extends HomegearNodeBase
         }
     }
 
-    public function stop()
-    {
+    public function stop(){
         if($this->mainHomegearChannel) $this->mainHomegearChannel->send(['name' => 'stop', 'value' => true]);
     }
 
-    public function waitForStop()
-    {
-        if($this->mainFuture)
-        {
+    public function waitForStop(){
+        if($this->mainFuture){
             $this->mainFuture->value();
             $this->mainFuture = NULL;
         }
 
-        if($this->mainHomegearChannel)
-        {
+        if($this->mainHomegearChannel){
             $this->mainHomegearChannel->close();
             $this->mainHomegearChannel = NULL;
         }
 
-        if($this->mainRuntime)
-        {
+        if($this->mainRuntime){
             $this->mainRuntime->close();
             $this->mainRuntime = NULL;
         }
